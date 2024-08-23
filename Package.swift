28a4affd528c2527db8167fdfb9160387daa2655
Dishi07 <90136830+Dishi07@users.noreply.github.com>
2024-08-23 17:14:20 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SampleiOS",
-            path: "SampleiOS.xcframework"
+            path: "./SampleiOS.xcframework"
         )
     ]
 )
