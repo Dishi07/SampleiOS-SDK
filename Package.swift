9e0818fe25eb11dc2fca5704cfa4243dd643b480
Dishi07 <90136830+Dishi07@users.noreply.github.com>
2024-08-25 11:39:47 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "SampleiOS-SDK",
+            name: "SampleiOS",
             path: "https://github.com/Dishi07/SampleiOS-SDK/tree/main/SampleiOS.xcframework",
             checksum: "630462758b50463c46711f382ddefdd7c0553ea70c9814956f81d89667e9ead0"
         )
