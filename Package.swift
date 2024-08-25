3faf597117fbba72f3ac6fcda6f72cd878fcb589
Dishi07 <90136830+Dishi07@users.noreply.github.com>
2024-08-25 11:39:35 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,17 +2,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "SampleiOS-SDK",
+    name: "SampleiOS",
     platforms: [.iOS(.v15)],
     products: [
         .library(
-            name: "SampleiOS-SDK",
-            targets: ["SampleiOS-SDK"]),
+            name: "SampleiOS",
+            targets: ["SampleiOS"]),
     ],
     targets: [
         .binaryTarget(
             name: "SampleiOS-SDK",
-            path: "SampleiOS.xcframework" // ローカルパスに変更
+            path: "https://github.com/Dishi07/SampleiOS-SDK/tree/main/SampleiOS.xcframework",
+            checksum: "630462758b50463c46711f382ddefdd7c0553ea70c9814956f81d89667e9ead0"
         )
     ]
 )
