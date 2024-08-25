d43c112be731379acedfe8b0bbb1944f4afc4365
Dishi07 <90136830+Dishi07@users.noreply.github.com>
2024-08-25 11:12:20 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SampleiOS-SDK",
-            path: "https://github.com/Dishi07/SampleiOS-SDK/releases/download/1.0.0/SampleiOS.xcframework.zip"
+            path: "SampleiOS.xcframework" // ローカルパスに変更
         )
     ]
 )
